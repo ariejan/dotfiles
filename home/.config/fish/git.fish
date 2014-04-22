b81a6c46f86b81d425ff973f560f59445aa5c4e4
Ariejan de Vroom <ariejan@ariejan.net>
2014-04-22 21:31:32 +0200

    Add git shortcuts

--- home/.config/fish/git.fish
+++ home/.config/fish/git.fish
@@ -1,14 +1,16 @@
 alias g=git
+alias gst="git status -sb"
 alias gb="git branch -a"
+alias glh="git log --graph --pretty=format:'%Cred%h%Creset %Cblue<%ae>%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gc="git commit"
+alias gco="git checkout"
+alias gb="git branch"
+alias gba="git branch -a"
+alias grv="git remote -v"
+alias gclr="git reset HEAD --hard ; git clean -fd"
+alias gz="git archive -o snapshot.zip HEAD"
+alias gt="git archive -o snapshot.tar.gz HEAD"
 
-function glh --description 'Show short git log'
-  git log --graph --pretty=format:'%Cred%h%Creset %Cblue<%ae>%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
-end
-
-function gst --description 'Show git status'
-  git status -sb
-end
-
-function gc --description 'Commit staged changes'
-  git commit
+function grm
+  git status | grep "deleted:" | awk '{print $2}' | xargs git rm --ignore-unmatch
 end
