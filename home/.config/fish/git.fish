d14b30a23c34849295d249333469104d7c881e74
Ariejan de Vroom <ariejan@ariejan.net>
2014-05-18 10:14:28 +0200

    Add alias hb for hub browse

--- home/.config/fish/git.fish
+++ home/.config/fish/git.fish
@@ -11,6 +11,8 @@ alias gclr="git reset HEAD --hard ; git clean -fd"
 alias gz="git archive -o snapshot.zip HEAD"
 alias gt="git archive -o snapshot.tar.gz HEAD"
 
+alias hb="hub browse"
+
 function grm
   git status | grep "deleted:" | awk '{print $2}' | xargs git rm --ignore-unmatch
 end
